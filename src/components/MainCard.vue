a10f860a317c238bfce623c77995e8b5f898464f
TontonBanton <pancla13ulas@gmail.com>
2024-06-27 15:19:23 +0800

    Initial Commit

---
+++ src/components/MainCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="main-container">
+    <div class="container-one">
+      <div class="profile">
+        <img src="../assets/profilepic.png" alt="">
+        <h3>Name Here</h3>
+        <p>Software Developer</p>
+      </div>
+      <div class="social-links">
+        <a href="">
+          <div class="link">
+            <img src="../assets/profilepic.png" alt="">
+            <h2>GitHub</h2>
+          </div>
+          <div class="link">
+            <img src="../assets/profilepic.png" alt="">
+            <h2>LinkdedIn</h2>
+          </div>
+          <div class="link">
+            <img src="../assets/profilepic.png" alt="">
+            <h2>Twitter</h2>
+          </div>
+          <div class="link">
+            <img src="../assets/profilepic.png" alt="">
+            <h2>Instagram</h2>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
