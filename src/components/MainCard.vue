e1e00cbe059154ee8554edb194e05c2dbf6fb488
TontonBanton <pancla13ulas@gmail.com>
2024-07-01 12:01:29 +0800

    Almost finish link to be added

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,96 +1,96 @@
 <template>
-  <div class="main-container">
-
-    <div class="container-one">
-      <div class="profile">
-        <img src="../assets/pic.jpg" alt="">
-        <h3>Name Here</h3>
-        <p>Software Developer</p>
-      </div>
-      <div class="social-links">
-        <div class="link">
-          <a href="">
-            <img src="../assets/github.png" alt="">
-            <h2>Github</h2>
-          </a>
+  <div class="wrapper">
+    <div class="main-container">
+      <div class="container-one">
+        <div class="profile">
+          <img src="../assets/pic.jpg" alt="">
+          <h2>Prestonn B. Ancla</h2>
+          <h3>Junior Software Developer</h3>
         </div>
-        <div class="link">
-          <a href="">
-            <img src="../assets/linkedin.png" alt="">
-            <h2>LinkedIn</h2>
-          </a>
-        </div>
-        <div class="link">
-          <a href="">
-            <img src="../assets/twitter.png" alt="">
-            <h2>Twitter</h2>
-          </a>
-        </div>
-        <div class="link">
-          <a href="">
-            <img src="../assets/instagram.png" alt="">
-            <h2>Instagram</h2>
-          </a>
+        <div class="social-links">
+          <div class="link">
+            <a href="">
+              <img src="../assets/github.png" alt="">
+              <h2>Github</h2>
+            </a>
+          </div>
+          <div class="link">
+            <a href="">
+              <img src="../assets/linkedin.png" alt="">
+              <h2>LinkedIn</h2>
+            </a>
+          </div>
+          <div class="link">
+            <a href="">
+              <img src="../assets/twitter.png" alt="">
+              <h2>Twitter</h2>
+            </a>
+          </div>
+          <div class="link">
+            <a href="">
+              <img src="../assets/instagram.png" alt="">
+              <h2>Instagram</h2>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-
-    <div class="container-two">
-    <div class="about-me">
-      <h1>About Me</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing
-        elit.Esse quidem iure magni incidunt quas. Doloremque
-        itaque delectusquae quaerat fugiat, id dolorum mollitia
-        sapiente, dicta cumque cupiditate et eveniet labore?
-      </p>
-    </div>
-    <div class="technology-section">
-      <h1>Technology Studied</h1>
-      <div class="logos">
-        <img src="../assets/js.png" alt="">
-        <img src="../assets/node.png" alt="">
-        <img src="../assets/vb.png" alt="">
-        <img src="../assets/sql.png" alt="">
-        <img src="../assets/css.png" alt="">
-        <img src="../assets/html.png" alt="">
-      </div>
-    </div>
-    <div class="button-container">
-      <div class="projects-button">
-        <a href="">My Projects</a>
-      </div>
-      <div class="projects-button">
-        <a href="">Curriculum Vitae </a>
+      <div class="container-two">
+        <div class="about-me">
+          <h1>About Me</h1>
+          <p> Enthusiastic Junior Front End Web Developer with a passion for
+              building responsive and user-friendly web applications. Proficient in
+              HTML, CSS, and JavaScript, with experience in popular framework like Vue.
+              Adept at collaborating with designers and backend developers to create
+              seamless and engaging user experiences. Committed to continuous learning
+              and staying updated with the latest web development trends.
+          </p>
+        </div>
+        <div class="technology-section">
+          <h1>Technology Studied</h1>
+          <div class="logos">
+            <img src="../assets/js.png" alt="">
+            <img src="../assets/node.png" alt="">
+            <img src="../assets/vb.png" alt="">
+            <img src="../assets/sql.png" alt="">
+            <img src="../assets/css.png" alt="">
+            <img src="../assets/html.png" alt="">
+          </div>
+        </div>
+        <div class="button-container">
+          <div class="projects-button"><a href="">My Projects</a></div>
+          <div class="projects-button"><a href="">Curriculum Vitae </a></div>
+        </div>
       </div>
     </div>
   </div>
-  </div>
-
-
 </template>
 
 <script>
 export default {
-
 }
 </script>
 
 <style scoped>
+  .wrapper{
+    display: flex;
+    justify-content: center;
+
+  }
   img {
     height: 20px;
   }
   .main-container {
+    width: 75%;
     display: flex;
     flex-direction: row;
-    background-color: gray;
     border-radius: 25px;
   }
-
   .container-one {
-    background-color: lightcoral;
     padding: 2.5%;
     width: 30%;
-    border-radius: 25px;
+    border-top-left-radius: 25px;
+    border-bottom-left-radius: 25px;
+    background-color: #7fc5fa;
   }
   .profile img {
     height: 5rem;
@@ -106,15 +106,19 @@ export default {
   }
 
   .container-two {
-    background-color: skyblue;
-    margin: 2.5%;
+    /* margin: 2.5%; */
+    padding: 1.5%;
     width: 70%;
-    /*border-radius: 25px;*/
+    border-top-right-radius: 25px;
+    border-bottom-right-radius: 25px;
+    background-color: rgb(106, 185, 238);
   }
+
   .about-me {
     text-align: left;
   }
   .logos img{
+    margin: 5px;
     height: 2.5rem;
   }
   .button-container {
@@ -123,16 +127,16 @@ export default {
     flex-direction: row;
     justify-content: center;  /* align horizontally */
     align-items: center;      /* align vertically */
-
   }
   .projects-button {
-    background-color:green;
+    width: 20rem;
     border-radius: 15px;
     padding: 5px;
     margin: 5px;
+    background-color: #0291ff;
   }
   .projects-button a{
-    color: white;
+    color: black;
     text-decoration: none;
   }
 </style>
---
+++ src/components/ProjectsCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <!--
+     {{ projectData.ProjectsArray }}
+  -->
+
+  <div class="main-container">
+    <div class="project-card" v-for="(items, index) in projectData.ProjectsArray" v-bind:key="items">     <!-- VFORLOOP-->
+      <h2>{{ projectData.ProjectsArray[index].Title }}</h2>
+      <img :src="projectData.ProjectsArray[index].Image" alt="">
+      <p>{{ projectData.ProjectsArray[index].About }}</p>
+      <div class="links">
+        <a :href="projectData.ProjectsArray[index].Link1" target="blank"><img src="../assets/github.png" alt=""></a>
+        <a :href="projectData.ProjectsArray[index].Link2" target="blank"><img src="../assets/link.png" alt=""></a>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+//import jsonData from"/projects.json"
+import jsonData from "../../projects.json"
+
+export default {
+  data() {
+    return { projectData: jsonData}
+  }
+}
+</script>
+
+<style scoped>
+  .main-container {
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+  .project-card {
+    margin: 10px;
+    padding: 10px;
+    flex-wrap: 0 0 40%;
+    background-color: white;
+    border-radius: 25px;
+    box-shadow: gray 0px 3px 8px;
+  }
+  .project-card img {
+    height: 10rem;
+  }
+  .links {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+  }
+  .links img {
+    height: 1.5rem;
+    margin: 5px;
+  }
+</style>
